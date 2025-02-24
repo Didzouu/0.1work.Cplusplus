dae45c69731c08e8c665d7fa46139e066f788d14
1ALAR1 <ior64@gmail.com>
2025-02-24 20:34:10 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	cout << "I have many pencils \n";
+	cout << "I have many pencils\n";
+	cout << "Red and green and blue.\n";
+	cout << "I can draw a picture\n";
+	cout << "And give it to you.\n";
+	cout << "I can draw my mother,\n";
+	cout << "I can draw my father\n";
+	cout << "I can draw my sister\n";
+	cout << "And my little brother\n";
+	cout << "i am autor\n";
+
+	cout << "Every\n hunter\n\t wants\n\t\t to\n\t\t\t know\n\t\t\t\t where\n\t\t\t\t\t the\n\t\t\t\t\t pheasant\n\t\t\t\t\t\t is.";
+
+
+	cout << "I will sell a chest of drawers\n";
+	cout << "colic brown\n";
+	cout << "phone: 067 - 992 - 94 - 00\n";
+	cout << "legth |1.5/0.5m|\n";
+	cout << "height |1m|\n";
+	cout << "the number of bedside tables |4|\n";
+
+}
+
+
+
+
