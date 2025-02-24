0d81b19bd032829a58d1db8f84fe580ff083dddc
1ALAR1 <ior64@gmail.com>
2025-02-24 20:56:25 +0200

    task 2 end 3

--- Source.cpp
+++ Source.cpp
@@ -4,26 +4,40 @@ using namespace std;
 
 void main()
 {
-	cout << "I have many pencils \n";
-	cout << "I have many pencils\n";
-	cout << "Red and green and blue.\n";
-	cout << "I can draw a picture\n";
-	cout << "And give it to you.\n";
-	cout << "I can draw my mother,\n";
-	cout << "I can draw my father\n";
-	cout << "I can draw my sister\n";
-	cout << "And my little brother\n";
-	cout << "i am autor\n";
+	//cout << "I have many pencils \n";
+	//cout << "I have many pencils\n";
+	//cout << "Red and green and blue.\n";
+	//cout << "I can draw a picture\n";
+	//cout << "And give it to you.\n";
+	//cout << "I can draw my mother,\n";
+	//cout << "I can draw my father\n";
+	//cout << "I can draw my sister\n";
+	//cout << "And my little brother\n";
+	//cout << "i am autor\n";
 
-	cout << "Every\n hunter\n\t wants\n\t\t to\n\t\t\t know\n\t\t\t\t where\n\t\t\t\t\t the\n\t\t\t\t\t pheasant\n\t\t\t\t\t\t is.";
+	//cout << "Every\n hunter\n\t wants\n\t\t to\n\t\t\t know\n\t\t\t\t where\n\t\t\t\t\t the\n\t\t\t\t\t pheasant\n\t\t\t\t\t\t is.";
 
 
-	cout << "I will sell a chest of drawers\n";
-	cout << "colic brown\n";
-	cout << "phone: 067 - 992 - 94 - 00\n";
-	cout << "legth |1.5/0.5m|\n";
-	cout << "height |1m|\n";
-	cout << "the number of bedside tables |4|\n";
+	//cout << "I will sell a chest of drawers\n";
+	//cout << "colic brown\n";
+	//cout << "phone: 067 - 992 - 94 - 00\n";
+	//cout << "legth |1.5/0.5m|\n";
+	//cout << "height |1m|\n";
+	//cout << "the number of bedside tables |4|\n";
+
+	//int diagonal;
+	//float Dyim = 2.54;
+	//cout << "diagonal : ";
+	//cin >>diagonal;
+	//cout << "diagonal = " <<diagonal * Dyim << endl;
+
+
+	float g;
+	int Deis = 30;
+	float kg = 1000;
+	cout << "g : ";
+	cin >> g;
+	cout << "g = " << g * Deis / kg << endl;
 
 }
 
